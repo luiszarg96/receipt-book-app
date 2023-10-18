5104f733259e1bfce93860737d9364ccf8ab35d6
Luis Zarza <luis.zarza01@gmail.com>
2023-10-18 15:40:03 -0300

    Initial Commit

---
+++ receipt-book/receipt_bookApp.swift
@@ -0,0 +1,20 @@
+//
+//  receipt_bookApp.swift
+//  receipt-book
+//
+//  Created by Luis Zarza on 2023-10-18.
+//
+
+import SwiftUI
+
+@main
+struct receipt_bookApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
