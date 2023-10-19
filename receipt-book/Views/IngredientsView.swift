6e85d423ee9ed790c323b924dd478746f717b390
Luis Zarza <luis.zarza01@gmail.com>
2023-10-19 16:22:35 -0300

    setup inicial

---
+++ receipt-book/Views/IngredientsView.swift
@@ -0,0 +1,20 @@
+//
+//  IngredientsView.swift
+//  receipt-book
+//
+//  Created by Luis Zarza on 2023-10-19.
+//
+
+import SwiftUI
+
+struct IngredientsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IngredientsView_Previews: PreviewProvider {
+    static var previews: some View {
+        IngredientsView()
+    }
+}
---
+++ receipt-book/Views/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding.swift
+//  receipt-book
+//
+//  Created by Luis Zarza on 2023-10-19.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
